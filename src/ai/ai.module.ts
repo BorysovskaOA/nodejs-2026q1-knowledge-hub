2ec182e230c3e14583c2c49fb0f2c7bd1a1d7a96
BorysovskaOA <olya.borysovska@gmail.com>
2026-05-07 21:32:01 +0300

    Removed useless controller import

--- src/ai/ai.module.ts
+++ src/ai/ai.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { GeminiModule } from './../gemini/gemini.module';
 import { AiService } from './ai.service';
 import { AiController } from './ai.controller';
-import { AiArticleController } from './article/ai.article.controller';
 import { AiArticleModule } from './article/ai.article.module';
 import { AiMonitoringModule } from './monitoring/ai.monitoring.module';
 
 @Module({
   imports: [GeminiModule, AiMonitoringModule, AiArticleModule],
-  controllers: [AiArticleController, AiController],
+  controllers: [AiController],
   providers: [AiService],
   exports: [AiService],
 })
